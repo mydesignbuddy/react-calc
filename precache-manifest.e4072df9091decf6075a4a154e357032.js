31394dd455bdd477b355e5b1f28ced137f826446
Buddy Toups <BuddyT@LAdmins-MacBook-Pro.local>
2019-07-10 17:15:38 -0500

    Updates

---
+++ precache-manifest.e4072df9091decf6075a4a154e357032.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8abd836cca587d01f5d093972827a7d7",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "7afdbb039ed94b1ace46",
+    "url": "/react-calc/static/css/main.a09dcfca.chunk.css"
+  },
+  {
+    "revision": "03d4b64d0dabd67dec8e",
+    "url": "/react-calc/static/js/2.6173085e.chunk.js"
+  },
+  {
+    "revision": "7afdbb039ed94b1ace46",
+    "url": "/react-calc/static/js/main.6d8485b2.chunk.js"
+  },
+  {
+    "revision": "48eedb427e7cd8bd6dd1",
+    "url": "/react-calc/static/js/runtime~main.0d723d41.js"
+  },
+  {
+    "revision": "e5a2022bfc835ef8c7bb6ff1e88beb8c",
+    "url": "/react-calc/static/media/bg.e5a2022b.jpg"
+  }
+]);
